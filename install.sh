e4546b250f2c59e073f98349b05b0a9428c3a681
Argh94 <hopgpli@gmail.com>
2025-06-01 13:08:56 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Colors for output
+red='\033[0;31m'
+green='\033[0;32m'
+cyan='\033[0;36m'
+rest='\033[0m'
+
+# Display installation header
+echo -e "${cyan}===========================================${rest}"
+echo -e "${cyan}|        Installing VlessIPOptimizer        |${rest}"
+echo -e "${cyan}| GitHub: ${green}https://github.com/Argh94${rest} |${rest}"
+echo -e "${cyan}===========================================${rest}"
+
+# Update and install Termux packages
+echo -e "${cyan}Updating and installing required Termux packages...${rest}"
+pkg update -y && pkg upgrade -y
+pkg install
